d5952373539cb62f37a1dc9f981e62281d71271a
csimmons20 <csimmons20@scienceleadership.org>
2018-09-21 13:26:22 -0400

    Initial commit

---
+++ src/org/sla/DrumKitTestDrive.java
@@ -0,0 +1,11 @@
+package org.sla;
+
+public class DrumKitTestDrive {
+
+    public static void main(String[] args) {
+	DrumKit d = new DrumKit();
+	d.snare = false;
+	d.playSnare();
+	d.playTopHat();
+    }
+}
